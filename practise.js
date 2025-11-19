09b821330cb8cbe7c3a5644e1bb9cb02bf59ef9c
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-11-19 21:57:59 +0600

    ...

--- practise.js
+++ practise.js
@@ -1,17 +1,78 @@
-class Book {
-  constructor(author) {
-    this._author = author;
+// class Book {
+//   constructor(author) {
+//     this._author = author;
+//   }
+//   // getter
+//   get writer() {
+//     return this._author;
+//   }
+//   // setter
+//   set writer(updatedAuthor) {
+//     this._author = updatedAuthor;
+//   }
+// }
+// const novel = new Book('anonymous');
+// console.log(novel.writer);
+// novel.writer = 'newAuthor';
+// console.log(novel.writer);
+
+class Person{
+  constructor(name, year_born){
+    this.name = name;
+    this.year_born = year_born;
   }
-  // getter
-  get writer() {
-    return this._author;
+
+  what(){
+    console.log(this.name + " is a person.");
   }
-  // setter
-  set writer(updatedAuthor) {
-    this._author = updatedAuthor;
+
+  static arms(){
+    return 2;
   }
 }
-const novel = new Book('anonymous');
-console.log(novel.writer);
-novel.writer = 'newAuthor';
-console.log(novel.writer);
+
+var me = new Person("Tanvir", 1998);
+// console.log(me.name + " was born in " + me.year_born + "!") ;
+// console.log(`${me.name} was born in ${me.year_born}`);
+
+// me.what();
+
+// console.log(`${me.name} has ${Person.arms()} arms`);
+
+class Juggler extends Person{
+  what(){
+    console.log(`${this.name} is a juggler.`);
+  }
+}
+
+var you = new Juggler("Masud", 1997);
+
+// me.what();
+// you.what();
+
+
+class Thermostat{
+  constructor(fahrenheit){
+    this._fahrenheit = fahrenheit;
+  }
+
+  get temperature(){
+    return (this._fahrenheit - 32) * 5/9;
+  }
+
+  // set temperature(celsius){
+  //    this._fahrenheit = (9/5 * celsius) + 32;
+  // }
+
+  // get fahrenheit(){
+  //   return this._fahrenheit; 
+  // }
+}
+
+let thermos = new Thermostat(76);
+console.log(thermos.temperature);
+
+thermos.temperature = 26;
+console.log(thermos.temperature);
+
+// console.log(thermos.fahrenheit);
