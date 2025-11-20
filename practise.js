221c5b3e63c896c6c5ba0e007c9634b44f6a4d1f
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-11-21 01:06:30 +0600

    ...

--- practise.js
+++ practise.js
@@ -61,20 +61,3 @@
 // console.log(thermos.fahrenheit);
 
 
-function template(strings, ...keys){
-  return function(...values){
-    let dict = values[values.length - 1] || {};
-    let results = [strings[0]];
-
-    keys.forEach(function(key,i){
-      var value = Number.isInteger(key) ? values[key] :  dict[key];
-      results.push(value,strings[i + 1]);
-    })
-  }
-}
-
-let t1Closure = template`${0}${1}${0}!`;
-console.log(t1Closure('Y','A'));
-
-let t2Closure = template`${0} ${'foo'}`;
-console.log(t2Closure('Hello', {foo: 'World'}));
