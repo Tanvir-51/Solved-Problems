a77ff65a6d7259f617bb150af339521eb74fd8e3
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-11-23 19:02:36 +0600

    ...

--- freeCodeCamp/Legacy JavaScript Algorithms and Data Structures V7/6_4.Find the Longest Word in a String.js
+++ freeCodeCamp/Legacy JavaScript Algorithms and Data Structures V7/6_4.Find the Longest Word in a String.js
@@ -27,3 +27,23 @@ function findLongestWordLength(str) {
 }
 
 findLongestWordLength('The quick brown fox jumped over the lazy dog');
+
+
+
+// alternative solutions
+
+2. 
+
+function findLongestWordLength(str) {
+  let words = str.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
