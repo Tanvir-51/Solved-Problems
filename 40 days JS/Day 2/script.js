e8022c22a758f46a7bda3fd3b052ae6585bfc249
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-11-26 01:26:54 +0600

    ...

--- 40 days JS/Day 2/script.js
+++ 40 days JS/Day 2/script.js
@@ -1,1 +1,34 @@
 console.log("Day 2");
+
+let name = "Tanvir";
+
+console.log(name);
+
+let age = 28;
+
+console.log(age);
+
+let isStudent = false;
+
+console.log(isStudent);
+
+let favLanguage = "JS";
+
+console.log(favLanguage);
+
+let student = {
+  name: "Tanvir",
+  age: 28,
+  isStudent: false,
+  favLanguage: "JS"
+};
+
+console.log(student);
+
+let studentInfo = student;
+
+console.log(studentInfo);
+
+let students = ["Tanvir", "Shihab", "Jubayer"];
+
+console.log(students);
