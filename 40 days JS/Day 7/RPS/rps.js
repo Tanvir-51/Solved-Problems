11c7e2c1d56d8e51f0ff54f6bf40f4bf077845dc
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-11-27 19:59:12 +0600

    RPS game

--- 40 days JS/Day 7/RPS/rps.js
+++ 40 days JS/Day 7/RPS/rps.js
@@ -7,12 +7,24 @@ function rockPaperScissorsGame() {
   let computerChoice;
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   
-  if (randomNumber === 1) {
-    computerChoice = "rock";
-  } else if (randomNumber === 2) {
-    computerChoice = "paper";
-  } else if (randomNumber === 3) {
-    computerChoice = 'scissors';
+  // if (randomNumber === 1) {
+  //   computerChoice = "rock";
+  // } else if (randomNumber === 2) {
+  //   computerChoice = "paper";
+  // } else if (randomNumber === 3) {
+  //   computerChoice = 'scissors';
+  // }
+
+  switch(randomNumber) {
+    case 1:
+      computerChoice = "rock";
+      break;
+    case 2:
+      computerChoice = "paper";
+      break;
+    case 3:
+      computerChoice = "scissors";
+      break;
   }
 
   console.log("User selected: ", userChoice);
@@ -26,6 +38,24 @@ function rockPaperScissorsGame() {
       console.log("You Win!!!");
   } else if(userChoice === computerChoice) {
       console.log("It's Draw!!");
+  } else if(
+    (userChoice === "rock" && computerChoice === "paper") ||
+    (userChoice === "paper" && computerChoice === "scissors") ||
+    (userChoice === "scissors" && computerChoice === "rock")
+  ) {
+    console.log("You Lose!!!");
+  }
+  else{
+      console.log("Please check the input again.");
+  }
+
+  const playAgainPrompt = prompt("Do you want to play again? (Yes/No)")
+  const playAgain = playAgainPrompt ? playAgainPrompt.toLowerCase() : 'No'
+
+  if(playAgain === "Yes") {
+    rockPaperScissorsGame();
+  } else {
+    console.log("Thanks for playing. See you next time!!");
   }
 }
 
