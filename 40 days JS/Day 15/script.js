0d9b37a22dffe800c7958d407206b45028f0a72e
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-12-04 23:44:33 +0600

    ...

--- 40 days JS/Day 15/script.js
+++ 40 days JS/Day 15/script.js
@@ -7,7 +7,7 @@ console.log(newCar.model);
 
 const salad = [
   "Tomato",
-  "mashroom",
+  "mushroom",
   "broccoly",
   "cabage",
   "corn",
@@ -17,7 +17,7 @@ const salad = [
 
 const anotherSalad = new Array(
   "Tomato",
-  "mashroom",
+  "mushroom",
   "broccoly",
   "cabage",
   "corn",
@@ -31,3 +31,45 @@ console.log("Constructor Salad: ", anotherSalad);
 for (let i = 0; i <= salad.length - 1; i++) {
   console.log(`Element at index ${i} is ${salad[i]}`);
 }
+
+const saladCopy = salad.slice();
+console.log("saladCopy: ", saladCopy);
+
+// destructuring
+
+const [tmt, mshrm, crt] = ["tomato", "mushroom", "carrot"];
+
+console.log(tmt, mshrm, crt);
+
+// rest parameter
+
+const [tomato, mushroom, ...rest] = [
+  "Tomato",
+  "mashroom",
+  "broccoly",
+  "cabage",
+  "corn",
+  "carrot",
+  "avocado",
+];
+
+// console.log(tomato, mushroom);
+console.log(rest);
+
+// swapping
+
+let first = "😟";
+let second = "😊";
+
+[first, second] = [second, first];
+
+console.log(first, second);
+
+// merge
+const emotions = ["😊", "🙁"];
+const veggies = ["cabage", "broccoly", "carrot"];
+
+const emotionalVeggies = [...emotions, ...veggies];
+
+console.log(emotionalVeggies);
+console.log(...emotionalVeggies);
