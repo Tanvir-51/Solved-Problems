fb9d716e5c1780021c48d354c72b289af1eb2e34
Tanvir-51 <tanvir19ahmed@gmail.com>
2025-12-04 21:56:15 +0600

    ...

--- 40 days JS/Day 15/script.js
+++ 40 days JS/Day 15/script.js
@@ -4,3 +4,30 @@ function Car(model) {
 
 let newCar = new Car("BMW X1");
 console.log(newCar.model);
+
+const salad = [
+  "Tomato",
+  "mashroom",
+  "broccoly",
+  "cabage",
+  "corn",
+  "carrot",
+  "avocado",
+];
+
+const anotherSalad = new Array(
+  "Tomato",
+  "mashroom",
+  "broccoly",
+  "cabage",
+  "corn",
+  "carrot",
+  "avocado"
+);
+
+console.log("Salad: ", salad);
+console.log("Constructor Salad: ", anotherSalad);
+
+for (let i = 0; i <= salad.length - 1; i++) {
+  console.log(`Element at index ${i} is ${salad[i]}`);
+}
